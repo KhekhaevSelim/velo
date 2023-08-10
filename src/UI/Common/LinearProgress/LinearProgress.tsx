541ac1cc1adcc89060290916a36bdd84e31250f1
SelimKhekhaev <selim16695@mail.ru>
2023-08-10 13:34:20 +0300

    added .env

--- src/UI/Common/LinearProgress/LinearProgress.tsx
+++ src/UI/Common/LinearProgress/LinearProgress.tsx
@@ -2,21 +2,6 @@ import React, { useEffect, useState } from "react";
 import style from "./LinaerProgress.module.css"
 
 const LinearProgress = () => {
-  /**
-   * изначально это была линия програсса))
-   */
-    // const [progressValue, setProgressValue] = useState<number>(0);
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //       setProgressValue(prevValue => (prevValue < 100 ? prevValue + 10 : prevValue));
-    //     });
-    //     if (progressValue === 100) {
-    //       clearInterval(interval);
-    //     }
-    //     return () => clearInterval(interval);
-    //   }, []);
-
     return (
         <div className={style.container}>
            <div className={style.line} >
