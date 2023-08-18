223336e489e3d4e3fb278c158f4e654d903f9a72
SelimKhekhaev <selim16695@mail.ru>
2023-08-18 19:50:35 +0300

    added unit-tests for authme abd logout actions

--- src/UnitTests/AuthReducer.test.ts
+++ src/UnitTests/AuthReducer.test.ts
@@ -1,4 +1,4 @@
-import { InitialStateType, authReducer, setUserProfileAC, setUserRegDataAC } from "../BLL/AuthReducer";
+import { InitialStateType, authReducer, logoutAC, meAC, setUserProfileAC, setUserRegDataAC } from "../BLL/AuthReducer";
 
 let startState : InitialStateType;
 beforeEach(()=> {
@@ -36,4 +36,36 @@ test("when user sign up regData should be contain him data", ()=> {
     expect(startState.regData).toBe(null);
     expect(endState.regData?.email).toBe("selim16695@mail.ru");
     expect(endState.regData?.code).toBe("qwerty12345");
+ })
+
+ test("when user sending auth me request we should be have identical user data and token null", ()=> {
+    let endState = authReducer(startState, meAC( {  
+        name : "Selim",
+        surname : "Khekhaev",
+        email : "selim16695@mail.ru",
+        is_activated : false,
+        created_at : "01.01.2023",
+        subscribes : []
+    }))
+ 
+    expect(startState).not.toEqual(endState);
+    expect(endState.profileData?.email).toBe("selim16695@mail.ru");
+ 
+ })
+
+ test("if user click logout his profile data shoulb be empty", ()=> {
+   startState = authReducer(startState, setUserProfileAC( {  
+        name : "Selim",
+        surname : "Khekhaev",
+        email : "selim16695@mail.ru",
+        is_activated : false,
+        created_at : "01.01.2023",
+        subscribes : [],
+        token : "qwerty123"
+     }))
+    let endState = authReducer(startState, logoutAC())
+ 
+    expect(startState.profileData).not.toEqual(endState.profileData);
+    expect(endState.profileData).toBe(null);
+ 
  })
