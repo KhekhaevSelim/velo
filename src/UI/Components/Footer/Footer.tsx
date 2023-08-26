ccc6afa4e776cfc435e45b7327274457a069da7c
SelimKhekhaev <selim16695@mail.ru>
2023-08-26 14:36:30 +0300

    add sendform page and footer markup

--- src/UI/Components/Footer/Footer.tsx
+++ src/UI/Components/Footer/Footer.tsx
@@ -25,14 +25,16 @@ const Footer = () => {
                     <a href="#upload">
                         Скачать
                     </a>
+                    <a href="#howStart">
+                        Как начать
+                    </a>
                 </div>
                 <div className={style.col3}>
-                    <a href="#howStart">Как начать</a>
                     <a href="#routes">Маршруты</a>
-                    <a href="#training">Тренировки</a>
-                    <a href="#routes">Маршруты</a>
+                    <a href="#training">Тренировки</a>                  
                     <a href="#about">О нас</a>
                     <a href="#help">Помощь</a>
+                    <a href="#help">Тарифы</a>
                 </div>
                 <div className={style.col4}>
                     <p>Связаться с нами</p>
